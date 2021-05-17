bd534af5572c679ef5361dea49e3db5b8aaf30a5
antobevi <abevilacqua@frba.utn.edu.ar>
2021-05-17 12:56:18 -0300

    Pequenios cambios que me olvide de subir

--- src/main/java/domain/Uniformes/Sastre.java
+++ src/main/java/domain/Uniformes/Sastre.java
@@ -2,12 +2,6 @@ package domain.Uniformes;
 
 import domain.Prenda;
 
-/*
-Elijo usar Factory Method porque me permite separar la construcción del uso y no necesito,
-al menos para los requerimientos actuales, la flexibilidad del Abstract Method al permitirle a otros objetos
-utilizar al Sastre para construir uniformes. Como desventaja principal está el uso de herencia que genera más
-acoplamiento y cohesión.
-*/
 public abstract class Sastre {
   public void crearUniforme() {
     new Uniforme(
